66e1ad450dd2bdfef3566025a76ba0e9b9ffb700
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-10-25 23:56:55 +0900

    first commit

---
+++ src/test/java/com/glidemap/app/GlideMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glidemap.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlideMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
